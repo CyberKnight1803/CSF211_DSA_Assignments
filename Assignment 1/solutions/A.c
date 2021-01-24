ba8e9484adef416d6c280fae6e63b9f0aa2ca8ea
CyberKnight1803 <omkarpitale2001@gmail.com>
2021-01-24 20:57:42 +0530

    Finally

--- Assignment 1/solutions/A.c
+++ Assignment 1/solutions/A.c
@@ -1,48 +1,51 @@
 #include<stdio.h>
 
-int flag = 1;
+int f = 1, m = 0, i = 0;
+char c;
 
-int matcher(char *s, char *p){
-    if(*s == '\0' && *p != '\0'){
-        return 0;
-    }
-    else if(*p == '\0'){
-        return 1;
-    }
-    else if(*s == *p){
-        return matcher(s + 1, p + 1);
-    }
-    else{
-        return 0;
-    }
-}
-
-
-void rec(int n, char *S){
-    static int i = 0;
-    if(matcher(S, "baab") && i < n){
-        if(flag){
-            printf("YES");
-            flag = 0;
+void rec(int n){
+    if(n != 0){
+        scanf("%c", &c);
+        if(c == 'b' && m == 0){
+            ++i;
+            ++m;
+            rec(n - 1);
         }
-        printf(" %d", i + 1);
-        i += 3;
-        rec(n, S + 3);
-    }
-    else if(i < n){
-        i += 1;
-        rec(n, S + 1);
+        else if(c == 'a' && m == 1){
+            ++i;
+            ++m;
+            rec(n - 1);
+        }
+        else if(c == 'a' && m == 2){
+            ++i;
+            ++m;
+            rec(n - 1);
+        }
+        else if(c == 'b' && m == 3){
+            ++i;
+            m = 1;
+            if(f){
+                printf("YES");
+                f = 0;
+            }
+            printf(" %d", i - 3);
+            rec(n -1);
+        }
+        else{
+            m = (c == 'b')?1:0;
+            ++i;
+            rec(n - 1);
+        }
     }
     else{
-        if(flag)
+        if(f){
             printf("NO");
+        }
     }
 }
 
 int main(){
     int n; scanf("%d", &n);
-    char S[n];
-
-    scanf("%s", S);
-    rec(n, S);
+    scanf("%c", &c);
+    rec(n);
 }
