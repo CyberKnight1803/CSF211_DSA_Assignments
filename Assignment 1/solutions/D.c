ed1858965d4538174770642db8489995ee869135
CyberKnight1803 <omkarpitale2001@gmail.com>
2021-01-24 23:55:35 +0530

    f

--- Assignment 1/solutions/D.c
+++ Assignment 1/solutions/D.c
@@ -3,17 +3,19 @@
 
 void generateBitString(int m, int n,  int *p, int *bitString){
     for(int i = 0; i < n; ++i){
-        bitString[p[i]] = 1; 
+        bitString[p[i]] += 1;  // freq
     }
 }
 
 int maxProducts(int m, int *bitString){
     int money = m, products = 0;
     for(int i = 0; i <= m; ++i){
-        if(bitString[i] == 1){
-            money -= i;
-            if(money > 0){
-                ++products;
+        if(bitString[i] >= 1){
+            for(int j = 1; j <= bitString[i]; ++j){
+                money -= i;
+                if(money >= 0){
+                    ++products;
+                }
             }
         }
         if(money < 0){
@@ -27,7 +29,7 @@ int main(){
     int m; scanf("%d", &m);
     int n; scanf("%d", &n);
     int *p = (int *)malloc(sizeof(int) * n);
-    int *bitString = (int *)malloc(sizeof(int) * (m + 1));
+    int *bitString = (int *)calloc((m + 1), sizeof(int));
 
     for(int i = 0; i < n;  ++i){
         scanf("%d", &p[i]);
