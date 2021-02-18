e1c005a7007dd64531668584bdccd6d521691784
CyberKnight1803 <omkarpitale2001@gmail.com>
2021-02-18 17:35:56 +0530

    c

--- Assignment_3/solutions/D.c
+++ Assignment_3/solutions/D.c
@@ -1,14 +1,53 @@
 #include<stdio.h>
 
+void swap(long *a, long *b){
+     long t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition(long *arr, int L, int H){
+    int pivot = arr[H];
+    int pIdx = L - 1;
+
+    for(int i = L; i <= H - 1; ++i){
+        if(arr[i] < pivot){
+            pIdx++;
+            swap(&arr[pIdx], &arr[i]);
+        }
+    }
+    swap(&arr[pIdx + 1], &arr[H]);
+    return pIdx + 1;
+}
+
+void qSort(long *arr, int L, int H){
+    if(L < H){
+        int pIdx = partition(arr, L, H);
+        qSort(arr, L, pIdx - 1);
+        qSort(arr, pIdx + 1, H);
+    }
+}
+
+void subArrs(int n, long *a, long *b){
+    for(int i = 0; i < n; ++i){
+        a[i] = a[i] - b[i];
+    }
+}
 
 int goodPairs(long *x, long *y, int n, int k1, int k2){
-    int pairs = 0;
+    subArrs(n, x, y);
+    qSort(x, 0, n - 1);
 
-    for(int i = 0; i < n; ++i){
-        for(int j = i + 1; j < n; ++j){
-            if(x[i] + x[j] + k1 > y[i] + y[j] + k2){
-                pairs++;
-            }
+
+    int pairs = 0, p1 = 0, p2 = n - 1;
+
+    while(p1 < p2){
+        if(x[p1] + x[p2] > k2 - k1){
+            pairs += p2 - p1;
+            p2--;
+        }
+        else{
+            p1++;
         }
     }
     return pairs;
