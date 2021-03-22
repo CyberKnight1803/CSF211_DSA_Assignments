6d83355179eb3ec581a21b62d399f0fbe1d46c51
CyberKnight1803 <omkarpitale2001@gmail.com>
2021-03-23 01:14:38 +0530

    more efficient

--- a6/solutions/f.c
+++ a6/solutions/f.c
@@ -1,36 +1,26 @@
 #include<stdio.h>
 
-void lRotate(int a[], int n, int t){
-    while(t--){
-        int data = a[0];
-        for(int i = 0; i < n - 1; ++i){
-            a[i] = a[i + 1];
-        }
-        a[n - 1] = data;
-    }
-}
-
-void solution(int a[], int n, int T[], int t){
-    for(int i = 0; i < t - 1; ++i){
-        lRotate(a, n, T[i]);
-        printf("%d %d\n", a[0], a[n - 1]);
-    }
-    lRotate(a, n, T[n - 1]);
-    printf("%d %d", a[0], a[n - 1]);
-}  
-
-
 int main(){
     int n, t;
     scanf("%d %d", &n, &t);
     
-    int a[n], T[t];
+    int a[n];
     for(int i = 0; i < n; ++i){
         scanf("%d", &a[i]);
     }
-    for(int i = 0; i < t; ++i){
-        scanf("%d", &T[i]);
-    }
 
-    solution(a, n, T, t);
+    int front = 0, rear = n - 1;
+
+    for(int i = 0; i < t; ++i){
+        int rotate;
+        scanf("%d", &rotate);
+
+        front = (front + rotate) % n;
+        rear = (rear + rotate) % n;
+
+        printf("%d %d", a[front], a[rear]);
+        if(i != t - 1){
+            printf("\n");
+        }
+    }
 }
